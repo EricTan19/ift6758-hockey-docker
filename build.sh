794b6a6cc1221eddc326a6acfaaf4b2906a26fee
Isaaruwu <ismailaarabb@gmail.com.com>
2025-12-07 18:06:27 -0500

    fix

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 docker build -t ift6758/serving:latest -f Dockerfile.serving .
+docker build -t ift6758/streamlit:latest -f Dockerfile.streamlit .
--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
-docker run -p 5000:5000 -e WANDB_API_KEY=${WANDB_API_KEY} ift6758/serving:latest
+NETWORK_NAME=ift6758-net
+docker network create "${NETWORK_NAME}"
+docker run -d --name serving --network "${NETWORK_NAME}" -p 5050:5000 -e WANDB_API_KEY="${WANDB_API_KEY}" ift6758/serving:latest
+docker run -d --name streamlit --network "${NETWORK_NAME}" -p 8501:8501 -e WANDB_API_KEY="${WANDB_API_KEY}" ift6758/streamlit:latest
